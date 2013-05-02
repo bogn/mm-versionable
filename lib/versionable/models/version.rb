d67f6478831863c4e111adb5384b8f3b565f4885
Bogomir Engel <bogomir.engel@gmx.de>
2013-05-02 17:52:32 +0200

    Accessing the content of a version with a dot path like "embedded_doc.key" is now supported. This enables the same for Model.diff(key, v1, v2).

--- lib/versionable/models/version.rb
+++ lib/versionable/models/version.rb
@@ -9,18 +9,43 @@ class Version
   key :message, String
   key :updater_id, ObjectId
   key :activity, String # e.g. 'created' or 'updated'
+  key :type, String
+
 
   def content(key)
-    cdata = self.data[key]
-    if cdata.respond_to?(:join)
-      cdata.join(" ")
+    if key.include?(".")
+      access_with_dot_syntax(key)
     else
-      cdata
+      cdata = self.data[key]
+      if cdata.respond_to?(:join)
+        cdata.join(" ")
+      else
+        cdata
+      end
     end
+
   end
 
 
   def self.create_indexes
     ensure_index [[:doc_id, 1], [:pos, -1]]
   end
+
+
+  private
+
+
+    def access_with_dot_syntax(key)
+      parts = key.split('.')
+      cdata = self.data[parts.shift] # get the first-level
+      parts.each do |p|
+        if p.to_i.to_s == p
+          cdata = cdata[p.to_i]
+        else
+          cdata = cdata[p.to_s] || cdata[p.to_sym]
+        end
+        break unless cdata
+      end
+      cdata
+    end
 end
